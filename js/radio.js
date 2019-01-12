b764dd02e3e09c83371931c6d385532b8da19154
WDaan <dw.daanwijns@gmail.com>
2019-01-12 11:29:43 +0100

    added a Radio player!!

---
+++ js/radio.js
@@ -0,0 +1,44 @@
+function TOPRADIO() {
+    $('#radio').attr('src', 'http://jwmp3.topradio.be/topradio.mp3');
+}
+function TOPRADIO_TOPSELECTION() {
+    $('#radio').attr('src', 'http://str.topradio.be/TOPradioArena.aac');
+}
+
+function QMUSIC_NONSTOP() {
+    $('#radio').attr('src', 'http://icecast-qmusic.cdp.triple-it.nl/Qmusic_be_non-stop_128.mp3');
+}
+
+function QMUSIC() {
+    $('#radio').attr('src', 'http://icecast-qmusic.cdp.triple-it.nl/Qmusic_be_live_128.mp3');
+}
+
+function Q_DANCE() {
+    $('#radio').attr('src', 'http://icecast-qmusic.cdp.triple-it.nl/Qmusic_be_dance_128.mp3');
+}
+
+function MNM() {
+    $('#radio').attr('src', 'http://icecast.vrtcdn.be/mnm-high.mp3');
+}
+
+function MNM_HITS() {
+    $('#radio').attr('src', 'http://icecast.vrtcdn.be/mnm_hits-mid.mp3');
+}
+
+function MNM_URBANICE() {
+    $('#radio').attr('src', 'http://icecast.vrtcdn.be/mnm_urb-high.mp3');
+}
+
+function STUBRU() {
+    $('#radio').attr('src', 'http://icecast.vrtcdn.be/stubru-high.mp3');
+}
+
+function KLARA() {
+    $('#radio').attr('src', 'http://icecast.vrtcdn.be/klara-high.mp3');
+}
+
+function JOE() {
+    $('#radio').attr('src', 'http://icecast-qmusic.cdp.triple-it.nl/JOEfm_be_live_128.mp3');
+}
+
+
