c6a2adae28dd93901e4019e2a99c68aae760cb95
WDaan <dw.daanwijns@gmail.com>
2019-01-14 10:08:35 +0100

    cleaned up radioselector code... 1 function with multiple objects instead of multiple functions

--- js/radio.js
+++ js/radio.js
@@ -1,69 +1,71 @@
-function TOPRADIO() {
-    $('#radio').attr('src', 'http://jwmp3.topradio.be/topradio.mp3');
+function set_station(station) {
     reset_selected();
-    $('#TOPRADIO').addClass("selected");
-}
-function TOPRADIO_TOPSELECTION() {
-    $('#radio').attr('src', 'http://str.topradio.be/TOPradioArena.aac');
-    reset_selected();
-    $('#TOPSELECTION').addClass("selected");
-}
-
-function QMUSIC_NONSTOP() {
-    $('#radio').attr('src', 'http://icecast-qmusic.cdp.triple-it.nl/Qmusic_be_non-stop_128.mp3');
-    reset_selected();
-    $('#Q_NONSTOP').addClass("selected");
-}
-
-function QMUSIC() {
-    $('#radio').attr('src', 'http://icecast-qmusic.cdp.triple-it.nl/Qmusic_be_live_128.mp3');
-    reset_selected();
-    $('#QMUSIC').addClass("selected");
-}
-
-function Q_DANCE() {
-    $('#radio').attr('src', 'http://icecast-qmusic.cdp.triple-it.nl/Qmusic_be_dance_128.mp3');
-    reset_selected();
-    $('#Q_DANCE').addClass("selected");
-}
-
-function MNM() {
-    $('#radio').attr('src', 'http://icecast.vrtcdn.be/mnm-high.mp3');
-    reset_selected();
-    $('#MNM').addClass("selected");
-}
-
-function MNM_HITS() {
-    $('#radio').attr('src', 'http://icecast.vrtcdn.be/mnm_hits-mid.mp3');
-    reset_selected();
-    $('#MNM_HITS').addClass("selected");
-}
-
-function MNM_URBANICE() {
-    $('#radio').attr('src', 'http://icecast.vrtcdn.be/mnm_urb-high.mp3');
-    reset_selected();
-    $('#MNM_URBANICE').addClass("selected");
-}
-
-function STUBRU() {
-    $('#radio').attr('src', 'http://icecast.vrtcdn.be/stubru-high.mp3');
-    reset_selected();
-    $('#STUBRU').addClass("selected");
-}
-
-function RHR() {
-    $('#radio').attr('src', 'http://stream.rhr.fm/');
-    reset_selected();
-    $('#RHR').addClass("selected");
-}
-
-function SLAM_HardStyle() {
-    $('#radio').attr('src', 'http://streaming.slam.nl/web11_mp3	');
-    reset_selected();
-    $('#SLAM!_HardStyle').addClass("selected");
+    $('#radio').attr('src', station.url);
+    $('#' + station.name).addClass("selected");
 }
 
 function reset_selected() {
     $('button').removeClass("selected");
 }
 
+
+/*     STATIONS     */
+var TOPRADIO = {
+    name: 'TOPRADIO',
+    url: 'http://jwmp3.topradio.be/topradio.mp3'
+};
+
+var TOPSELECTION = {
+    name: 'TOPSELECTION',
+    url: 'http://str.topradio.be/TOPradioArena.aac'
+};
+
+var QMUSIC = {
+    name: 'QMUSIC',
+    url: 'http://icecast-qmusic.cdp.triple-it.nl/Qmusic_be_live_128.mp3'
+};
+
+var QMUSIC_NONSTOP = {
+    name: 'QMUSIC_NONSTOP',
+    url: 'http://icecast-qmusic.cdp.triple-it.nl/Qmusic_be_non-stop_128.mp3'
+};
+
+var MNM = {
+    name: 'MNM',
+    url: 'http://icecast.vrtcdn.be/mnm-high.mp3'
+};
+
+var MNM_HITS = {
+    name: 'MNM_HITS',
+    url: 'http://icecast.vrtcdn.be/mnm_hits-mid.mp3'
+};
+
+var MNM_URBANICE = {
+    name: 'MNM_URBANICE',
+    url: 'http://icecast.vrtcdn.be/mnm_urb-high.mp3'
+};
+
+var STUBRU = {
+    name: 'STUBRU',
+    url: 'http://icecast.vrtcdn.be/stubru-high.mp3'
+};
+
+var Q_DANCE = {
+    name: 'Q_DANCE',
+    url: 'http://icecast-qmusic.cdp.triple-it.nl/Qmusic_be_dance_128.mp3'
+}
+
+var RHR = {
+    name: 'RHR',
+    url: 'http://stream.rhr.fm/'
+};
+
+var SLAM_HardStyle = {
+    name: 'SLAM_HardStyle',
+    url: 'http://streaming.slam.nl/web11_mp3'
+};
+
+var DANCE_UK = {
+    name: 'DANCE_UK',
+    url: 'http://212.83.150.15:8022/stream/'
+};
