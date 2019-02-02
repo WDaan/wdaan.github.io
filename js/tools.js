2a4a504d507f1e751c50c0b9cbea1464784dd583
WDaan <dw.daanwijns@gmail.com>
2019-02-02 19:55:34 +0100

    bug fix, server info: not shows server offline before trying to fetch data

--- js/tools.js
+++ js/tools.js
@@ -1,9 +1,9 @@
 var ProgressBar = require('progressbar.js')
 var line = new ProgressBar.Line('#container');
 
-//eerst checken of server online is
+//check if server offline & init circles for offline
 var offline = true;
-
+var circle = [];
 
 function check_server() {
     var http = new XMLHttpRequest();
@@ -14,13 +14,11 @@ function check_server() {
         console.log("Server timed out ...");
         check_server();
         console.log("checking again...");
-        set_circles();
     }
 
     http.onerror = function (e) {
         offline = true;
         console.log("Server offline :/");
-        set_circles();
     };
 
     http.open("GET", "https://wdaan.me", /*async*/ true);
@@ -28,7 +26,7 @@ function check_server() {
         if (http.readyState == 4 && http.status == 200) {
             console.log("Server online! ^__^");
             offline = false;
-            set_circles();
+            request_data();
         }
     };
     try {
@@ -37,83 +35,76 @@ function check_server() {
 };
 
 check_server();
+init_circles();
+
+function request_data() {
+    $.ajax({
+        'type': "GET",
+        'global': false,
+        'dataType': 'json',
+        'url': "https://wdaan.me/mijn_info/info.php",
+        'data': {
+            'request': "",
+            'target': 'arrange_url',
+            'method': 'method_target'
+        },
+        'success': function (data) {
+            //console.log(data);
+            save_values(data);
+        },
+    });
+};
+
+function save_values(info) {
+    var total = info[0];
+    var free = info[1];
+    var media = info[2];
+    var daan = info[3];
+    document.getElementById('container').setAttribute("data-value", free);
+    document.getElementById('container').setAttribute("text", 'Free');
+    document.getElementById('container2').setAttribute("data-value", daan);
+    document.getElementById('container2').setAttribute("text", 'Daan');
+    document.getElementById('container3').setAttribute("data-value", media);
+    document.getElementById('container3').setAttribute("text", 'Media');
+    set_progress(total);
+};
 
 
-var total;
-var free;
-var daan;
-var media;
 
-function set_circles() {
-    if (!offline) {
-        $.ajax({
-            'type': "GET",
-            'global': false,
-            'dataType': 'json',
-            'url': "https://wdaan.me/mijn_info/info.php",
-            'data': {
-                'request': "",
-                'target': 'arrange_url',
-                'method': 'method_target'
-            },
-            'success': function (data) {
-                //console.log(data);
-                save_values(data);
-            },
+function init_circles() {
+    $('.progress').each(function (i) {
+        circle[i] = new ProgressBar.Circle(this, {
+            color: '#64a19d',
+            strokeWidth: 4,
+            trailWidth: 2,
+            easing: 'easeInOut',
+            duration: 1400
         });
+        circle[i].setText("Server offline");
+    })
+};
 
 
-        function save_values(info) {
-            total = info[0];
-            free = info[1];
-            media = info[2];
-            daan = info[3];
-            document.getElementById('container').setAttribute("data-value", free);
-            document.getElementById('container').setAttribute("text", 'Free');
-            document.getElementById('container2').setAttribute("data-value", daan);
-            document.getElementById('container2').setAttribute("text", 'Daan');
-            document.getElementById('container3').setAttribute("data-value", media);
-            document.getElementById('container3').setAttribute("text", 'Media');
-            set_progress(total);
-        }
 
-        /* progress bar */
-        function set_progress(total) {
-            $('.progress').each(function (i) {
-                var circle = new ProgressBar.Circle(this, {
-                    color: '#64a19d',
-                    strokeWidth: 4,
-                    trailWidth: 2,
-                    easing: 'easeInOut',
-                    duration: 1400
-                });
-                var value = ($(this).attr('data-value') / total);
-                var type = $(this).attr('text');
-                var amount = $(this).attr('data-value');
-                circle.animate(value, {
-                    from: {
-                        color: '#64a19d'
-                    },
-                    to: {
-                        color: '#64a19d'
-                    },
-                    step: function (state, circle) {
-                        circle.path.setAttribute('stroke', state.color);
-                        circle.setText(type + "\n" + amount + 'GB');
-                    }
-                });
-                circle.text.style.fontSize = '1.3em';
-            });
-
-        };
-
-    } else {
-        $('.progress').each(function (i) {
-            var circle = new ProgressBar.Circle(this, {});
-
-            circle.setText("Server offline");
-            circle.text.style.fontSize = '1.4em';
-        })
-    }
+/* progress bar */
+function set_progress(total) {
+    $('.progress').each(function (i) {
+        var value = ($(this).attr('data-value') / total);
+        var type = $(this).attr('text');
+        var amount = $(this).attr('data-value');
+        circle[i].animate(value, {
+            from: {
+                color: '#64a19d'
+            },
+            to: {
+                color: '#64a19d'
+            },
+            step: function (state, circle) {
+                circle.path.setAttribute('stroke', state.color);
+                circle.setText(type + "\n" + amount + 'GB');
+            }
+        });
+        circle[i].text.style.fontSize = '1.3em';
+    });
 
 };
