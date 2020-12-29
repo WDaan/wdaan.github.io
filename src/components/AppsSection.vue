b58909b7cfdffc743174506b68110905c53d2c51
Daan Wijns <dw.daanwijns@gmail.com>
2020-12-29 17:00:29 +0100

    basic homepage

--- src/components/AppsSection.vue
+++ src/components/AppsSection.vue
@@ -1,39 +1,37 @@
 <template>
-  <section class="py-10 bg-gray-50">
-    <div class="pb-12 pt-4 w-10/12 rounded mx-auto bg-white shadow-md">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="lg:text-center">
-          <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            Apps
-          </p>
-        </div>
+  <section class="w-full pb-12 pt-4  rounded mx-auto bg-white shadow-md">
+    <div class=" mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="lg:text-center">
+        <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          Apps
+        </p>
+      </div>
 
-        <div class="mt-10">
-          <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-            <a
-              style="cursor: pointer"
-              target="_blank"
-              v-for="app in apps"
-              :key="app.name"
-              @click="openUrl(app.url)">
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <img :src="app.icon"/>
-                  </div>
-                </div>
-                <div class="ml-4">
-                  <dt class="text-lg leading-6 font-medium text-gray-900">
-                    {{ app.name }}
-                  </dt>
-                  <dd class="mt-2 text-base text-gray-500">
-                    {{ app.description }}
-                  </dd>
+      <div class="mt-10">
+        <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+          <a
+            style="cursor: pointer"
+            target="_blank"
+            v-for="app in apps"
+            :key="app.name"
+            @click="openUrl(app.url)">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <img :src="app.icon"/>
                 </div>
               </div>
-            </a>
-          </dl>
-        </div>
+              <div class="ml-4">
+                <dt class="text-lg leading-6 font-medium text-gray-900">
+                  {{ app.name }}
+                </dt>
+                <dd class="mt-2 text-base text-gray-500">
+                  {{ app.description }}
+                </dd>
+              </div>
+            </div>
+          </a>
+        </dl>
       </div>
     </div>
   </section>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <AppsSection />
+    <div class="w-full m-0 p-0 bg-cover bg-bottom header">
+      <div class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
+        <p class="text-white font-extrabold text-3xl md:text-5xl">
+          Yeet
+        </p>
+      </div>
+    </div>
+
+    <!--Container-->
+    <div class="container px-4 md:px-0 max-w-6xl mx-auto -mt-64 md:-mt-32">
+      <div class="mx-0 sm:mx-6">
+        <div class="bg-gray-200 w-10/12 mx-auto text-xl md:text-2xl text-gray-800 leading-normal rounded-t">
+          <!--Lead Card-->
+          <AppsSection />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,3 +30,8 @@ export default defineComponent({
   components: { AppsSection }
 })
 </script>
+<style lang="css" scoped>
+.header {
+  background-image:url('~@/assets/cover.jpg'); height: 60vh; max-height:460px;
+}
+</style>
