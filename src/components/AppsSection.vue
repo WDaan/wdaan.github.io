305a083cbe3cc3859e80e31fc277274eba90501e
Daan Wijns <dw.daanwijns@gmail.com>
2020-12-29 10:12:25 +0100

    begin homepage

--- src/components/AppsSection.vue
+++ src/components/AppsSection.vue
@@ -1,28 +1,50 @@
 <template>
-  <section id="Apps" class="contact-section bg-black">
-    <div class="container">
-      <h2 class="text-uppercase mb-0 mx-auto text-center"></h2>
-      <div class="row">
-        <div
-          class="col-md-4 mb-3 mb-md-0 mt-5"
-          v-for="app in apps"
-          :key="app.name"
-        >
-          <div
-            class="card card1 py-4 h-100"
-            style="cursor: pointer"
-            @click="openUrl(app.url)"
-          >
-            <div class="card-body text-center">
-              <h4 class="text-uppercase m-0 txt">
-                {{ app.name }}
-              </h4>
-              <hr class="my-4 mx-auto" />
-              <div class="small text-black-50 txt">
-                {{ app.description }}
+  <section class="py-10 bg-gray-50">
+    <div class="pb-12 pt-4 w-10/12 rounded mx-auto bg-white shadow-md">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="lg:text-center">
+          <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Apps
+          </p>
+        </div>
+
+        <div class="mt-10">
+          <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            <a
+              target="_blank"
+              v-for="app in apps"
+              :key="app.name"
+              @click="openUrl(app.url)">
+              <div class="flex">
+                <div class="flex-shrink-0">
+                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <!-- Heroicon name: globe-alt -->
+                    <svg
+                      class="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                    </svg>
+                  </div>
+                </div>
+                <div class="ml-4">
+                  <dt class="text-lg leading-6 font-medium text-gray-900">
+                    {{ app.name }}
+                  </dt>
+                  <dd class="mt-2 text-base text-gray-500">
+                    {{ app.description }}
+                  </dd>
+                </div>
               </div>
-            </div>
-          </div>
+            </a>
+          </dl>
         </div>
       </div>
     </div>
@@ -51,11 +73,6 @@ export default defineComponent({
           name: 'Plex',
           url: 'https://app.plex.tv',
           description: 'Films en Series streamen van overal'
-        },
-        {
-          name: 'Qbit',
-          url: 'https://qbitreg.wdaan.xyz',
-          description: 'Remote downloaden van overal'
         },
         {
           name: 'Portainer',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,5 @@
 <template>
   <div>
-    <header class="masthead">
-      <div class="container d-flex h-100 align-items-center">
-        <div class="mx-auto text-center">
-          <h1 class="mx-auto my-0 text-uppercase">Yeet</h1>
-          <a
-            href="#Apps"
-            class="btn btn-primary white--text mt-5"
-          >
-            Get Started
-          </a
-          >
-        </div>
-      </div>
-    </header>
     <AppsSection />
   </div>
 </template>
